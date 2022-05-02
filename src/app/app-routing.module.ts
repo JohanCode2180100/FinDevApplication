03d64386fbcb9143c11c73a8ed13b5938a50cd76
JohanCode2180100 <saintyves.johan@gmail.com>
2022-05-02 23:27:07 +0200

    Add routing for inscrition and login pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageDashboardComponent } from './pages/page-dashboard/page-dashboard.component';
+import { PageInscriptionComponent } from './pages/page-inscription/page-inscription.component';
+import { PageLoginComponent } from './pages/page-login/page-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: PageLoginComponent,
+  },
+  {
+    path: 'inscription',
+    component: PageInscriptionComponent,
+  },
+  // {
+  //   path: '',
+  //   component: PageDashboardComponent,
+  //   children: [
+  //     {
+  //       path: 'articles',
+  //     },
+  //   ],
+  // },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
